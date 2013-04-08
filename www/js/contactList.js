f5db4ccaf45fcd0e522ec05255f4eae7f6b7dca5
Chris Kiang <chriskiang63@gmail.com>
2013-04-08 14:09:25 -0400

    MANUAL LABOR

--- www/js/contactList.js
+++ www/js/contactList.js
@@ -1,3 +1,4 @@
+function startJS(){
 var db;
 var dbCreated = false;
 
@@ -82,4 +83,5 @@ function populateDB(tx) {
         tx.executeSql("INSERT INTO organizations (organization_name, base_number) VALUES (`UMD`, `301305`), " +
 		      "(`Other Place`, `1234567890,`)");
 
+}
 }
