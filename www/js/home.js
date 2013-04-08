2af75e7d0be9020e7e4199d2a19ddd166c98bc38
Jacob Endres <demontongue@gmail.com>
2013-04-08 01:22:05 -0400

    FUCK YESSSSSSSSSSSS

--- www/js/home.js
+++ www/js/home.js
@@ -2,17 +2,17 @@ var serviceURL = "http://localhost/directory/services/";
 
 var phoneBook;
 
-$('#home').bind('pageinit', function(event) {
+$("#home").live("pagecreate", function(event) {
+	console.log("FUCK EVERYTHING");
 	getBooks();
 });
 
 function getBooks() {
 	$.getJSON(serviceURL + 'getBooks.php', function(data) {
-		$.each('#bookList li').remove();
+		$('#bookList li').remove();
 		phoneBook = data.items;
 		$.each(phoneBook, function(index, phoneBook) {
-			$('#bookList').append('<li><a href="numberList.html?id=' + phonebook.baseNumber + '">' +
-					   phoneBook.name   '</li>')
+			$('#bookList').append('<li><a href="numberList.html?id=' + phonebook(index).baseNumber + '">' + phoneBook(index).name  + '</li>');
 		});
 		$('#bookList').listview('refresh');
 	});
